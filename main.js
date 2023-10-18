47c491d6513037e0e567d13abf31094f90a93e68
raaji01 <raajalakshumik18077@it.ssn.edu.in>
2023-10-18 13:55:01 -0500

    final commit

--- main.js
+++ main.js
@@ -4,6 +4,29 @@ var btn= document.getElementById("btn");
 var light= document.getElementById("light");
 var logo=document.getElementById("logo");
 let popup= document.getElementById("popup")
+function ball1left() {
+  // Function to move Tom to the left
+  document.getElementById('ball1').style.left = 0;
+  //document.getElementById('ball1').style.imageOrientation = "https://img.cartoongoodies.com/wp-content/uploads/2019/10/16155814/tom-running-fast.png";
+
+}
+
+function ball1right() {
+  // Function to move Tom to the right
+  document.getElementById('ball1').style.left = "80vw";
+  /* document.getElementById('ball1').style.backgroundColor="forestgreen";*/
+}
+function ball2left() {
+  // Function to move Jerry to the left
+  document.getElementById('ball2').style.left = 0;
+  /* document.getElementById('ball2').style.backgroundColor = "blue";*/
+
+}
+function ball2right() {
+  // Function to move Jerry to the right
+  document.getElementById('ball2').style.left = "82vw";
+  /* document.getElementById('ball2').style.backgroundColor ="pink" ;*/
+}
 
 function toggleBtn(){
     btn.classList.toggle("active");
